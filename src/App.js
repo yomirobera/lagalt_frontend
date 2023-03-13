fce9dc036ba697c04754ecc134293beac1f1ff6c
“Mebrahtu” <mebzerizghi@gmail.com>
2023-03-13 10:18:05 +0100

    added header and search functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
+import LandingView from './Views/LandingView';
+import LoginView from './Views/LoginView';
+import ProfileView from './Views/ProfileView';
+import NavBar from './components/NavBar/NavBar';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Lagalt</h1>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+          <NavBar />
+            <Routes>
+              <Route path="/" element={ <LandingView />}/>
+              <Route path="/login" element={ <LoginView />}/>
+              <Route path="/profile" element={ <ProfileView />}/>
+            </Routes>
+        </div>
+    </BrowserRouter>
+    
   );
 }
 
