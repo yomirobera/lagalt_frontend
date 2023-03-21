807974eb0cfb126623943f9ba4f4daccc71b2cfd
Meb <mebzeritzghi@gmail.com>
2023-03-21 01:57:06 +0100

    landing page project list css  styles

--- src/components/projectList/projectList.jsx
+++ src/components/projectList/projectList.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { API_URL } from '../../api/projects';
 import { fetchProjectList } from '../../redux/actions'
 import keycloak from '../keycloak/keycloak';
-import musicImg from "../../assets/img/logo.png";
+import musicImg from "../../assets/img/musicImg.png";
 
 const ProjectList = () => {
   // useState hook
@@ -29,31 +29,39 @@ const { data, isSearching } = useSelector(state => state.projects);
   } else { 
     return (
       <Card>
-        <h2>Populære prosjekter</h2>
+        <h2 className='cardHeader'>Populære prosjekter</h2>
           {data.map(project => (
              <React.Fragment key={project.id}>
-              <Row gutter={16} justify="center" align="middle" style={{ minHeight: '60vh' }}>
-                <Col xs={24} sm={16} md={12} lg={8}>
-                  <h2>{project.category}</h2>
-                  <h3>{project.title}</h3>
-                  <div>
+              <Row gutter={16} style={{marginLeft: '0px', marginRight: '0px', paddingLeft: '25px'}}>
+                <Col xs={24} sm={12} md={14} lg={14} className={project.category.replace(' ', '-').toLowerCase()}
+                    style={{ paddingLeft: '50px',paddingTop:'25px', paddingRight: '0px'}}
+                  >
+                  <h2 className='categoryText'>{project.category} <span id='musicIcon' className={project.category.replace(' ', '-').toLowerCase()}></span></h2>
+                  <h3 className='projTitle'>{project.title}</h3>
+                  <div className='projTags'>
                       {project.skillsRequired.map(skill => (
-                        <Tag className={project.category.replace(' ', '-').toLowerCase()} style={{ borderRadius: 20, margin: '5px' }}>{skill}</Tag>
+                        <Tag className={project.category.replace(' ', '-').toLowerCase()} style={{ borderRadius: 20, margin: '5px', color:'white' }}>{skill}</Tag>
                       ))}
                   </div>
-                  <p><strong>Beskrivelse av prosjektet: </strong>{project.description}</p>
-                  <p><strong>ønskede ferdighter: </strong>{project.description}</p>
-                  <div>
+                  <p><strong>Beskrivelse av prosjektet: </strong>{project.description}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut </p>
+                  <p><strong>Ønskede ferdighter: </strong>{project.description}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut </p>
+                  <div className='reqSkills'>
                       {project.skillsRequired.map(skill => (
-                        <Tag  style={{ borderRadius: 20, margin: '5px' }}>{skill}</Tag>
+                        <Tag className='skills' style={{ borderRadius: 20, margin: '5px' }}>{skill}</Tag>
                       ))}
                   </div>
-                  <p><strong>Vil du bli på dette prosjektet? </strong>
-                  <a href="#" onClick={() => keycloak.login()}>Logg inn, </a>eller 
-                  <a href="#" onClick={() => keycloak.register()}> register bruker</a></p>
+                  <div className='card-loginLink'>
+                      <p><strong>Vil du bli på dette prosjektet? </strong>
+                      <a href="#" onClick={() => keycloak.login()}>Logg inn, </a>eller 
+                      <a href="#" onClick={() => keycloak.register()}> register bruker</a></p>
+                  </div>
                 </Col>
-                <Col xs={24} sm={12} style={{ marginBottom: 20 }}>
-                  <img alt="project cover" src={musicImg /* project.img_url */} style={{ maxWidth: '100%' }} />
+                <Col xs={24} sm={12} md={10} lg={10} style={{paddingLeft: '0px',
+                    paddingRight: '0px'}}>
+                  <img alt="project cover" src={musicImg /* project.img_url */} style={{ width: '100%',maxWidth: '100%',height:'100%', maxHeight: '100%', objectFit: 'cover'}} />
                 </Col>
               </Row>
             </React.Fragment>
