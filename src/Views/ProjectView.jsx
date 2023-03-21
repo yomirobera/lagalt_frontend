bd3e0b1e98acb5c83a42bc2a36dda62612b4edd5
YRober <yomirobera@gmail.com>
2023-03-21 13:17:32 +0100

    fixed some navigation problems

--- src/Views/ProjectView.jsx
+++ src/Views/ProjectView.jsx
@@ -1,10 +1,12 @@
 import EditProject from "../components/editProject/EditProject"
+import Profile from "../components/profile/Profile"
 
 const ProjectView = () => {
     return (
         <>
         <h1>Projects</h1>
-        <EditProject/>
+        
+        <Profile/>
         </>
         
     )
--- src/components/Buttons/LoginSignupBtn.jsx
+++ src/components/Buttons/LoginSignupBtn.jsx
@@ -2,10 +2,6 @@ import { Button,Space,AudioOutlined } from "antd";
 import { LoginOutlined, UserAddOutlined } from "@ant-design/icons";
 import keycloak from "../keycloak/keycloak";
 
-<<<<<<< HEAD
-
-import '../../css/LoginButton.css'
-=======
 /* const suffix = (
   <AudioOutlined
     style={{
@@ -21,29 +17,10 @@ const LoginSignupBtn = () => (
                     <Button className='btnLogin'  type="primary" ghost onClick={() => keycloak.login()}>
                         LOG INN
                     </Button>
->>>>>>> 2884826a9f87e0134df1165f9b6bdced4a2b0cb2
 
                     <Button className='btnSignup' type="primary" ghost onClick={() => keycloak.register()}>
                         NY BRUKER
                     </Button>
                   </>)}</Space>)
-/* const LoginSignupBtn = () => {
-  return (
-    <div>
-      {!keycloak.authenticated && (
-        <Button className="btnFunction" icon={<LoginOutlined />} 
-          onClick={() => keycloak.login()}>
-          Login
-        </Button>
-      )}
 
-      {!keycloak.authenticated && (
-        <Button className="btnFunction" icon={<UserAddOutlined />} 
-          onClick={() => keycloak.register()}>
-          Signup
-        </Button>
-      )}
-    </div>
-    
-  ); */
  export default LoginSignupBtn;
