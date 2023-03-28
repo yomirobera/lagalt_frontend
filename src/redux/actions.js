c6190ea05b58300e5d2d10faef1f3c2312111583
Kenny Lu <kenny.lu@no.experis.com>
2023-03-28 10:26:34 +0200

    actions.js recommended proj

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,7 @@
 import { API_URL } from '../api/projects';
-import { fetchProjects } from '../redux/projectsReducer'
+import keycloak from '../components/keycloak/keycloak';
+import { fetchProjects } from '../redux/projectsReducer';
+import {fetchSkills} from '../redux/projectsReducer';
 
 export const setSearchQuery = (query) => ({
     type: 'SET_SEARCH_QUERY',
@@ -22,8 +24,12 @@ export const clearallFilter = (clear) => ({
 });
 
 export const fetchProjectList = () => {
+    let url = API_URL;
+    if(keycloak.authenticated){
+        url = `http://localhost:8080/api/v1/users/${keycloak.tokenParsed.sub}/getRecommendedProjects`;
+    }
     return (dispatch, getState) => {
-        fetch(API_URL) // make a GET request to the API
+        fetch(url) // make a GET request to the API
             .then(res => res.json()) 
             .then(
                 result => { 
@@ -33,5 +39,33 @@ export const fetchProjectList = () => {
                 	dispatch({type: "FETCH_DATA_ERROR", payload: error})
                 }
             );
+    }
+}
+
+export const fetchUserSkills = () => {
+    console.log('what is happening here: reaching here?')
+    if(keycloak.authenticated){
+        const API_URL_SKILL = `http://localhost:8080/api/v1/users/${keycloak.tokenParsed.sub}/getAllSkills`;
+      
+    console.log(API_URL_SKILL);
+    return (dispatch, getState) => {
+        fetch(API_URL_SKILL) // make a GET request to the API
+            .then(res =>res.json()) 
+            .then(
+                result => { 
+                    if (result){
+                        dispatch(fetchSkills(result))
+                        console.log(result)
+                   } else {
+                    console.log('Empty response from API')
+                   // dispatch(fetchSkills([]))
+                    }     
+                }, 
+                error => { // handle any errors
+                	console.log('Error fetching skills:', error)
+                   // dispatch(fetchSkills([]))
+                }
+            );
+       }
     }
 }
